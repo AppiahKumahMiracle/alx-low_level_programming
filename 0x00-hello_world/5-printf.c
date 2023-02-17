dc79871ee0345db52e82b8b5b6c5af1e081fbaeb
AppiahKumahMiracle <codewithklick@gmail.com>
2023-02-16 21:19:12 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - am wirtting a program that display a message to the user using the printf function.
+* Return:0 (Run successful)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
