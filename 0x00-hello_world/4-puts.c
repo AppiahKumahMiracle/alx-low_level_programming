dece4e962cd9e1276de02c0a1f0641e3832f0ed5
AppiahKumahMiracle <codewithklick@gmail.com>
2023-02-16 20:49:49 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program that print or display using the put function
+ * Return:0 (Run successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
