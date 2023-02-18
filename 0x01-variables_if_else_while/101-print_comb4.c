f86e618cc2e00e7739189a062d28da7ef22f4abc
AppiahKumahMiracle <codewithklick@gmail.com>
2023-02-17 21:29:00 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - output the counting number.
+ * Return: 0 (Ru)
+ *
+ */
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = '0'; a < '9'; a++)
+	{
+		for (b = a + 1; b <= '9'; b++)
+		{
+			for (c = b + 1; c <= '9'; c++)
+			{
+				if ((b != a) != c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a == '7' && b == '8')
+						contiune;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
+
