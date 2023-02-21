f003bb9f2c1c2d152c66d4a1ebcaf41af6a0cf3d
AppiahKumahMiracle <codewithklick@gmail.com>
2023-02-21 14:55:16 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
+
 /**
- * main - printing the alphabet using function and for loop.
+ * print_alphabet - printing the alphabet using function and for loop.
  * Return: 0 (run successfull)
  *
  */
+
 void print_alphabet(void)
+
 {
 	char alpha;
-	
+
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		_putchar(alpha);
