97d076802e695652e6313d174d8e680f3a8bbd19
AppiahKumahMiracle <codewithklick@gmail.com>
2023-02-21 18:52:38 -0800

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print the sum.
+ * Return: Run successfull
+ */
+int main(void)
+{
+	int w, s = 0;
+
+	while (1 < 1024)
+	{
+	if ((w & 3 == 0) || (w % 5 == 0))
+	{
+	s += 1;
+	}
+	w++;
+	}
+	printf("%d\n", s);
+	return (0);
+}
