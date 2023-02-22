c121d8ee709579b4a572acbdbb8572ec013771d6
AppiahKumahMiracle <codewithklick@gmail.com>
2023-02-21 17:08:15 -0800

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/***
+ * print_sign - prints the sign in number.
+ * @n: number to check
+ * Return: 1 as positive number, -1 as negative and 0 as anything.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar(43);
+	return (1);
+	}
+	else if (n < 0)
+	{
+	_putchar(45);
+	return (-1);
+	}
+	else
+	{
+	_putchar(48);
+	return (0);
+	}
+}
