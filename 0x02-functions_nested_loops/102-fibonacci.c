935a6f084158cb0b439f346108a6dd0e6c2b4d08
AppiahKumahMiracle <codewithklick@gmail.com>
2023-02-22 20:11:51 -0800

    103-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -15,12 +15,12 @@ int main(void)
 	if (num == 0)
 	printf("%ld\n", Lnum);
 	else if (num == 1)
-	printf(", %ld", Knum);
+	printf(",%ld", Knum);
 	else
 	{
 	Knum += Lnum;
 	Lnum = Knum - Lnum;
-	printf(", %ld", Knum);
+	printf(",%ld", Knum);
 	}
 	++num;
 	}
